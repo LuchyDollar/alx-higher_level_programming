3fb60b0c78393ec97dcdbd17689923aa96f3fae2
LuchyDollar <oluchireke@yahoo.com>
2022-12-08 20:04:27 +0000

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+_dictionary, value):
+    keys_to_del = []
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            keys_to_del.append(key)
+    for key in keys_to_del:
+        del a_dictionary[key]
+    return a_dictionary
