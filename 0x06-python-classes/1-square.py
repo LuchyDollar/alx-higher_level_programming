c5523820b59c0c29eefcc493f1c13e446d48a72f
LuchyDollar <oluchireke@yahoo.com>
2022-12-24 08:22:43 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creating a new class called Square"""
+
+
+class Square:
+    """class Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        """Initialize Square with size attribute"""
+        self.__size = size
